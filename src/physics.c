52d7bf084c790c1e329e64d1f9d7a22712f1a7d1
Sarah Burns <mystical.unicat@gmail.com>
2021-05-27 16:16:56 -0700

    auto

--- src/physics.c
+++ src/physics.c
@@ -13,6 +13,13 @@ alias_ecs_Result alias_Physics2DBundle_initialize(alias_ecs_Instance * instance,
         transform_bundle->Translation2D_component
       , bundle->Physics2DParticle_component
       }
+#if ALIAS_TRANSFORM_ENABLE_HEIRARCHY
+    , .num_filters = 1
+    , .filters = &(alias_ecs_QueryFilterCreateInfo) {
+        .filter = ALIAS_ECS_FILTER_EXCLUDE
+      , .component = transform_bundle->Parent2D_component
+      }
+#endif
   }, &bundle->particle_query);
 
   return ALIAS_ECS_SUCCESS;
@@ -30,11 +37,17 @@ static void _update2d_particles(void * ud, alias_ecs_Instance * instance, alias_
   alias_R duration = udata->duration;
   alias_Translation2D * translation = (alias_Translation2D *)data[0];
   alias_Physics2DParticle * particle = (alias_Physics2DParticle *)data[1];
-
+  translation->value.x = alias_fma(particle->velocity.x, duration, translation->value.x);
+  translation->value.y = alias_fma(particle->velocity.y, duration, translation->value.y);
+  alias_Vector2D total_acceleration = {
+      .x = alias_fma(particle->accumulated_force.x, particle->inverse_mass, particle->acceleration.x
+    , .y = alias_fma(particle->accumulated_force.y, particle->inverse_mass, particle->acceleration.y
+    };
 }
 
 void alias_physics_update2d_particles(alias_ecs_Instance * instance, alias_Physics2DBundle * bundle, alias_R duration) {
-  alias_ecs_execute_query(instance, bundle->particle_query, (alias_ecs_QueryCB) { _construct_local_from_translation, bundle });
+  struct _update2d_particles_data udata = { .duration = duration };
+  alias_ecs_execute_query(instance, bundle->particle_query, (alias_ecs_QueryCB) { _update2d_particles, &udata});
 }
 
 // =============================================================================================================================================================
