0627a8ed3fc736c4354988775ff315af98bdae10
Sarah Burns <mystical.unicat@gmail.com>
2021-05-27 14:46:52 -0700

    auto

--- src/physics.c
+++ src/physics.c
@@ -1,3 +1,10 @@
 #include <alias/physics.h>
 
+alias_ecs_Result alias_Physics2DBundle_initialize(alias_ecs_Instance * instance, alias_Physics2DBundle * bundle, alias_TransformBundle * transform_bundle) {
+  alias_ecs_register_component(instance, &(alias_ecs_ComponentCreateInfo) {
+  }, alias_ecs_ComponentHandle *component_ptr
+}
+
+void alias_Physics2DBundle_update2d_serial(alias_ecs_Instance * instance, alias_Physics2DBundle * bundle) {
+}
 
