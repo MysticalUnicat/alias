9a169964efbc45936225c46ee9feb7f2af74df88
Sarah Burns <mystical.unicat@gmail.com>
2021-05-27 14:59:40 -0700

    auto

--- include/alias/physics.h
+++ include/alias/physics.h
@@ -23,7 +23,7 @@ typedef struct alias_Physics2DBundle {
 } alias_Physics2DBundle;
 
 alias_ecs_Result alias_Physics2DBundle_initialize(alias_ecs_Instance * instance, alias_Physics2DBundle * bundle, alias_TransformBundle * transform_bundle);
-void alias_Physics2DBundle_update2d_serial(alias_ecs_Instance * instance, alias_Physics2DBundle * bundle);
+void alias_Physics2DBundle_update2d_serial(alias_ecs_Instance * instance, alias_Physics2DBundle * bundle, alias_R duration);
 
 #endif
 
