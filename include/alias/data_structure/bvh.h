889b4eaa4188e93ab99734c9a426bec3b40b107a
Sarah Burns <mystical.unicat@gmail.com>
2021-06-09 10:45:23 -0700

    auto Wed Jun  9 10:45:23 AM PDT 2021

--- include/alias/data_structure/bvh.h
+++ include/alias/data_structure/bvh.h
@@ -2,6 +2,19 @@
 #define _ALIAS_DATA_STRUCTURE_BVH_H_
 
 #include <alias/data_structure/aabb.h>
+#include <stdint.h>
+
+typedef struct alias_BVH2D_Node {
+  int32_t      p, b, c;
+  alias_AABB2D A, B, C;
+} alias_BVH2D_Node;
+
+typedef struct alias_BVH2D {
+  uint32_t num_nodes;
+  alias_BVH2D_Node * nodes;
+} alias_BVH2D;
+
+#if 0
 #include <alias/data_structure/soa.h>
 
 /*           (P)        */
@@ -23,13 +36,13 @@ typedef struct alias_BVH2D_NodeRef {
 } alias_BVH2D_NodeRef;
 
 #define ALIAS_BVH2D_NODE_X(X) \
-  X(uint32_t,               P_row) \
+  X(uint32_t,               P_ref) \
   /* A_row is the current row */ \
   /* A_box is constructed from B_box and C_box */ \
   X(alias_AABB2D,           B_box) \
   X(alias_AABB2D,           C_box) \
-  X(alias_BVH2D_NodeRef,    B_row) \
-  X(alias_BVH2D_NodeRef,    C_row) \
+  X(alias_BVH2D_NodeRef,    B_ref) \
+  X(alias_BVH2D_NodeRef,    C_ref) \
   /* {D,E,F,G}_{row,box} are looked up */
 
 #define X(A, B) alias_BVH2D_NodeColumn_##B,
@@ -61,10 +74,10 @@ static inline uint32_t alias_BVH2D__allocate_node(alias_BVH2D * bvh, alias_Memor
 
 static inline void alias_BVH2D__insert(alias_BVH2D * bvh, alias_MemoryAllocationCallback * cb, uint32_t leaf_row, alias_AABB2D leaf_box) {
   // stage 1: find best sibling
-  alias_BVH2D_NodeRef * A_row = &bvh->root
-                    , * P_row = NULL, * B_row = NULL, * C_row = NULL;
-  alias_AABB2D B_box = *(const alias_AABB2D *)alias_SOA_read(&bvh->nodes, A_row->value, alias_BVH2D_NodeColumn_B_box)
-             , C_box = *(const alias_AABB2D *)alias_SOA_read(&bvh->nodes, A_row->value, alias_BVH2D_NodeColumn_C_box)
+  alias_BVH2D_NodeRef * A_ref = &bvh->root
+                    , * P_ref = NULL, * B_ref = NULL, * C_ref = NULL, * D_ref = NULL, * E_ref = NULL, * F_ref = NULL, * G_ref = NULL;
+  alias_AABB2D B_box = *(const alias_AABB2D *)alias_SOA_read(&bvh->nodes, A_ref->value, alias_BVH2D_NodeColumn_B_box), D_box, E_box
+             , C_box = *(const alias_AABB2D *)alias_SOA_read(&bvh->nodes, A_ref->value, alias_BVH2D_NodeColumn_C_box), F_box, G_box
              , A_box = alias_AABB2D_union(B_box, C_box)
              , AL_box = alias_AABB2D_union(A_box, leaf_box)
              ;
@@ -73,11 +86,11 @@ static inline void alias_BVH2D__insert(alias_BVH2D * bvh, alias_MemoryAllocation
         , dSU_AL = SU_AL - SU_A
         ;
   alias_BVH2D_NodeColumn P_child_column;
-  while(A_row->is_node) {
-    B_row = (alias_BVH2D_NodeRef *)alias_SOA_write(&bvh->nodes, A_row->value, alias_BVH2D_NodeColumn_B_row);
-    C_row = (alias_BVH2D_NodeRef *)alias_SOA_write(&bvh->nodes, A_row->value, alias_BVH2D_NodeColumn_C_row);
-    B_box = *(const alias_AABB2D *)alias_SOA_read(&bvh->nodes, A_row->value, alias_BVH2D_NodeColumn_B_box);
-    C_box = *(const alias_AABB2D *)alias_SOA_read(&bvh->nodes, A_row->value, alias_BVH2D_NodeColumn_C_box);
+  while(A_ref->is_node) {
+    B_ref = (alias_BVH2D_NodeRef *)alias_SOA_write(&bvh->nodes, A_ref->value, alias_BVH2D_NodeColumn_B_row);
+    C_ref = (alias_BVH2D_NodeRef *)alias_SOA_write(&bvh->nodes, A_ref->value, alias_BVH2D_NodeColumn_C_row);
+    B_box = *(const alias_AABB2D *)alias_SOA_read(&bvh->nodes, A_ref->value, alias_BVH2D_NodeColumn_B_box);
+    C_box = *(const alias_AABB2D *)alias_SOA_read(&bvh->nodes, A_ref->value, alias_BVH2D_NodeColumn_C_box);
     alias_AABB2D BL_box = alias_AABB2D_union(B_box, leaf_box)
                , CL_box = alias_AABB2D_union(C_box, leaf_box);
     alias_R SU_B = alias_AABB2D_surface_area(B_box)
@@ -87,22 +100,22 @@ static inline void alias_BVH2D__insert(alias_BVH2D * bvh, alias_MemoryAllocation
           , dSU_BL = SU_BL - SU_B
           , dSU_CL = SU_CL - SU_C
           , A_cost = SU_AL
-          , B_cost = (B_row->is_node ? dSU_BL : SU_BL) + dSU_AL
-          , C_cost = (C_row->is_node ? dSU_CL : SU_CL) + dSU_AL;
+          , B_cost = (B_ref->is_node ? dSU_BL : SU_BL) + dSU_AL
+          , C_cost = (C_ref->is_node ? dSU_CL : SU_CL) + dSU_AL;
     if(A_cost < B_cost && A_cost < C_cost) {
       break;
     }
-    P_row = A_row;
+    P_ref = A_ref;
     if(B_cost < C_cost) {
       P_child_column = alias_BVH2D_NodeColumn_B_row;
-      A_row = B_row;
+      A_ref = B_ref;
       A_box = B_box;
       AL_box = BL_box;
       SU_AL = SU_BL;
       dSU_AL = dSU_BL;
     } else {
       P_child_column = alias_BVH2D_NodeColumn_C_row;
-      A_row = C_row;
+      A_ref = C_ref;
       A_box = C_box;
       AL_box = CL_box;
       SU_AL = SU_CL;
@@ -118,25 +131,104 @@ static inline void alias_BVH2D__insert(alias_BVH2D * bvh, alias_MemoryAllocation
   /*  (B)       (C)    (A)    (L) */
   uint32_t NP_index = alias_BVH2D__allocate_node(bvh, cb);
 
-  *(uint32_t *)alias_SOA_write(&bvh->nodes, NP_index, alias_BVH2D_NodeColumn_P_row) = P_row->index;
+  *(uint32_t *)alias_SOA_write(&bvh->nodes, NP_index, alias_BVH2D_NodeColumn_P_row) = P_ref->index;
   *(alias_AABB2D *)alias_SOA_write(&bvh->nodes, NP_index, alias_BVH2D_NodeColumn_B_box) = B_box;
   *(alias_AABB2D *)alias_SOA_write(&bvh->nodes, NP_index, alias_BVH2D_NodeColumn_C_box) = C_box;
-  *(alias_BVH2D_NodeRef *)alias_SOA_write(&bvh->nodes, NP_index, alias_BVH2D_NodeColumn_B_row) = *A_row;
+  *(alias_BVH2D_NodeRef *)alias_SOA_write(&bvh->nodes, NP_index, alias_BVH2D_NodeColumn_B_row) = *A_ref;
   *(alias_BVH2D_NodeRef *)alias_SOA_write(&bvh->nodes, NP_index, alias_BVH2D_NodeColumn_C_row) = (alias_BVH2D_NodeRef) { .index = leaf_row };
 
   alias_BVH2D_NodeRef NP_row = (alias_BVH2D_NodeRef) { .is_node = 1, .index = NP_index };
 
-  *(alias_BVH2D_NodeRef *)alias_SOA_write(&bvh->nodes, A_row->index, alias_BVH2D_NodeColumn_P_row) = NP_row;
+  *(alias_BVH2D_NodeRef *)alias_SOA_write(&bvh->nodes, A_ref->index, alias_BVH2D_NodeColumn_P_row) = NP_row;
   *(alias_BVH2D_NodeRef *)alias_SOA_write(&bvh->nodes, leaf_row, alias_BVH2D_NodeColumn_P_row) = NP_row;
 
-  if(P_row != NULL) {
-    *(alias_BVH2D_NodeRef *)alias_SOA_write(&bvh->nodes, P_row->index, P_child_column) = NP_row;
+  if(P_ref != NULL) {
+    *(alias_BVH2D_NodeRef *)alias_SOA_write(&bvh->nodes, P_ref->index, P_child_column) = NP_row;
   } else {
     bvh->root = NP_row;
   }
 
   // stage 3: refitting
-  
+  /*           (A)        */
+  /*        /      \      */
+  /*     (B)       (C)    */
+  /*    /  \      /  \    */
+  /* (D)   (E) (F)    (G) */
+  enum {
+    NONE,
+    DE,
+    FG
+  } known = NONE;
+  B_ref = A_ref;
+  B_box = A_box;
+  C_ref = &(alias_BVH2D_NodeRef) { .index = leaf_row };
+  C_box = leaf_box;
+  A_ref = &NP_row;
+  A_box = alias_AABB2D_union(B_box, C_box);
+  while(A_ref->value != 0) {
+    alias_R B_noswap_score = alias_AABB2D_surface_area(C_box);
+    alias_R BF_swap_score = alias_R_MAX;
+    alias_R BG_swap_score = alias_R_MAX;
+
+    alias_R C_noswap_score = alias_AABB2D_surface_area(B_box);
+    alias_R CD_swap_score = alias_R_MAX;
+    alias_R CE_swap_score = alias_R_MAX;
+    
+    if(C_ref->is_node) {
+      // B <-> {F,G} is possible
+      if(known != FG) {
+        F_ref = (alias_BVH2D_NodeRef *)alias_SOA_write(&bvh->nodes, C_ref->index, alias_BVH2D_NodeColumn_B_ref);
+        G_ref = (alias_BVH2D_NodeRef *)alias_SOA_write(&bvh->nodes, C_ref->index, alias_BVH2D_NodeColumn_C_ref);
+        F_box = *(alias_AABB2D *)alias_SOA_write(&bvh->nodes, C_ref->index, alias_BVH2D_NodeColumn_B_box);
+        G_box = *(alias_AABB2D *)alias_SOA_write(&bvh->nodes, C_ref->index, alias_BVH2D_NodeColumn_C_box);
+      }
+
+      BF_swap_score = alias_AABB2D_surface_area(alias_AABB2D_union(B_box, G_box));
+      BG_swap_score = alias_AABB2D_surface_area(alias_AABB2D_union(B_box, F_box));
+    }
+
+    if(!B_ref->is_node) {
+      // C <-> {D,E} is possible
+      if(known != DE) {
+        D_ref = (alias_BVH2D_NodeRef *)alias_SOA_write(&bvh->nodes, B_ref->index, alias_BVH2D_NodeColumn_B_ref);
+        E_ref = (alias_BVH2D_NodeRef *)alias_SOA_write(&bvh->nodes, B_ref->index, alias_BVH2D_NodeColumn_C_ref);
+        D_box = *(alias_AABB2D *)alias_SOA_write(&bvh->nodes, B_ref->index, alias_BVH2D_NodeColumn_B_box);
+        E_box = *(alias_AABB2D *)alias_SOA_write(&bvh->nodes, B_ref->index, alias_BVH2D_NodeColumn_C_box);
+      }
+
+      CD_swap_score = alias_AABB2D_surface_area(alias_AABB2D_union(B_box, E_box));
+      CE_swap_score = alias_AABB2D_surface_area(alias_AABB2D_union(B_box, D_box));
+    }
+
+    alias_R best_B_swap = ALIAS_MIN_PAIR(BF_swap_score, BG_swap_score);
+    bool swap_B = B_noswap_score > best_B_swap;
+
+    alias_R best_C_swap = ALIAS_MIN_PAIR(CD_swap_score, CE_swap_score);
+    bool swap_C = C_noswap_score > best_C_swap;
+
+    if(swap_B && swap_C) {
+      *(alias_BVH2D_NodeRef *)alias_SOA_write(&bvh->nodes, B_ref->index, alias_BVH2D_NodeColumn_P_ref)
+      if(best_B_swap < best_B_swap) {
+        swap_C = false;
+      } else {
+        swap_B = false;
+      }
+    }
+
+    if(swap_B) {
+      if(BF_swap_score < BG_swap_score) {
+      }
+      if(BF_swap_score > BG_swap_score) {
+      }
+    }
+
+    if(swap_C) {
+      if(CD_swap_score < CE_swap_score) {
+      }
+      if(CD_swap_score > CE_swap_score) {
+      }
+    }
+  }
 }
 
 static inline void alias_BVH2D_initialize(alias_BVH2D * bvh, alias_MemoryAllocationCallback * cb) {
@@ -162,221 +254,6 @@ static inline void alias_BVH2D_move_leaf(alias_BVH2D * bvh, alias_MemoryAllocati
 static inline void alias_BVH2D_remove_leaf(alias_BVH2D * bvh, uint32_t leaf_node) {
   alias_BVH2D__remove(bvh, leaf_node);
 }
-
-#if 0
-struct extracted_node {
-  alias_ecs_EntityHandle   entity;
-  alias_Physics2DBVHNode * node;
-};
-
-static inline struct extracted_node _extract(alias_ecs_Instance * instance, alias_ecs_ComponentHandle component, alias_ecs_EntityHandle entity) {
-  struct extracted_node enode;
-  enode.entity = entity;
-  alias_ecs_write_entity_component(instance, entity, component, (void **)&enode.node);
-  return enode;
-}
-
-struct sibling_analysis {
-  alias_AABB2D union_with_leaf;
-  alias_R      union_delta;
-};
-
-static struct sibling_analysis _analyze_node(
-    struct extracted_node enode
-  , alias_AABB2D          leaf_aabb
-) {
-  struct sibling_analysis analysis;
-  analysis.union_with_leaf = alias_AABB2D_union(enode.node->aabb, leaf_aabb);
-  analysis.union_delta = analysis.union_with_leaf.surface_area - enode.node->aabb.surface_area;
-  return analysis;
-}
-
-static inline void _insert(
-    alias_ecs_Instance        * instance
-  , alias_ecs_ComponentHandle   component
-  , alias_ecs_EntityHandle    * root
-  , alias_ecs_EntityHandle      _leaf
-) {
-
-  // stage 1: find best sibling
-  struct extracted_node leaf = _extract(instance, component, _leaf);
-
-  struct extracted_node parent, sibling, child1, child2;
-  struct sibling_analysis parent_a, sibling_a, child1_a, child2_a;
-
-  parent = _extract(instance, component, *root);
-  parent_a = _analyze_node(parent, leaf.node->aabb);
-
-  sibling = parent;
-  sibling_a = parent_a;
-
-  while(!sibling.node->leaf) {
-    child1 = _extract(instance, component, parent.node->child_1);
-    child2 = _extract(instance, component, parent.node->child_2);
-
-    child1_a = _analyze_node(child1, leaf.node->aabb);
-    child2_a = _analyze_node(child2, leaf.node->aabb);
-
-    alias_R sibling_cost = sibling_a.union_with_leaf.surface_area;
-    alias_R child1_cost = (child1.node->leaf ? child1_a.union_with_leaf.surface_area : child1_a.union_delta) + sibling_a.union_delta;
-    alias_R child2_cost = (child2.node->leaf ? child2_a.union_with_leaf.surface_area : child2_a.union_delta) + sibling_a.union_delta;
-
-    if(sibling_cost < child1_cost && sibling_cost < child2_cost) {
-      break;
-    }
-
-    parent = sibling;
-    if(child1_cost < child2_cost) {
-      sibling = child1;
-    } else {
-      sibling = child2;
-    }
-  }
-
-  // stage 2: create new parent
-  alias_ecs_EntityHandle _new_parent;
-  alias_ecs_spawn(
-      instance
-    , &(alias_ecs_EntitySpawnInfo) {
-        .count = 1
-      , .num_components = 1
-      , .components = (alias_ecs_EntitySpawnComponent[]) {
-          { .component = component
-          , .data = (const void **)&(alias_Physics2DBVHNode) {
-              .aabb = sibling_a.union_with_leaf
-            , .parent = parent.entity
-            , .child_1 = sibling.entity
-            , .child_1 = _leaf
-            }
-          }
-        }
-      }
-    , &_new_parent);
-
-  sibling.node->parent = _new_parent;
-  leaf.node->parent = _new_parent;
-
-  if(sibling.entity != 0) {
-    if(parent.node->child_1 == sibling.entity) {
-      parent.node->child_1 = _new_parent;
-    } else {
-      parent.node->child_2 = _new_parent;
-    }
-  } else {
-    *root = _new_parent;
-  }
-
-  parent = _extract(instance, component, _new_parent);
-
-  // stage 3:
-  //   walk back up the tree refitting AABBs
-  //   in addition - we are balancing the tree as we go
-  // 
-  //           (A)        |
-  //        /      \      |
-  //     (B)       (C)    |
-  //    /  \      /  \    |
-  // (D)   (E) (F)    (G) |
-  // four possible rotations: B <-> F , B <-> G , C <-> D , C <-> E
-  struct extracted_node
-      A = parent
-    , B = child1, D, E
-    , C = child2, F, G
-    ;
-  enum {
-    NONE,
-    DE,
-    FG
-  } known = NONE;
-  while(parent.entity != 0) {
-    alias_R B_noswap_score = C.node->aabb.surface_area;
-    alias_R BF_swap_score = alias_R_MAX;
-    alias_R BG_swap_score = alias_R_MAX;
-
-    alias_R C_noswap_score = B.node->aabb.surface_area;
-    alias_R CD_swap_score = alias_R_MAX;
-    alias_R CE_swap_score = alias_R_MAX;
-    
-    if(!C.node->leaf) {
-      // B <-> {F,G} is possible
-      if(known != FG) {
-        F = _extract(instance, component, C.node->child_1);
-        G = _extract(instance, component, C.node->child_2);
-      }
-
-      BF_swap_score = alias_AABB2D_union(B.node->aabb, G.node->aabb).surface_area;
-      BG_swap_score = alias_AABB2D_union(B.node->aabb, F.node->aabb).surface_area;
-    }
-
-    if(!child1.node->leaf) {
-      // C <-> {D,E} is possible
-      if(known != DE) {
-        D = _extract(instance, component, B.node->child_1);
-        E = _extract(instance, component, B.node->child_2);
-      }
-
-      CD_swap_score = alias_AABB2D_union(B.node->aabb, E.node->aabb).surface_area;
-      CE_swap_score = alias_AABB2D_union(B.node->aabb, D.node->aabb).surface_area;
-    }
-
-    alias_R best_B_swap = ALIAS_MIN_PAIR(BF_swap_score, BG_swap_score);
-    bool swap_B = B_noswap_score > best_B_swap;
-
-    alias_R best_C_swap = ALIAS_MIN_PAIR(CD_swap_score, CE_swap_score);
-    bool swap_C = C_noswap_score > best_C_swap;
-
-    if(swap_B && swap_C) {
-      if(best_B_swap < best_B_swap) {
-        swap_C = false;
-      } else {
-        swap_B = false;
-      }
-    }
-
-    struct extracted_node temp;
-
-    if(swap_B) {
-      C.node->child_1 = B.entity;
-      B.node->parent = C.entity;
-      if(BF_swap_score < BG_swap_score) {
-        F.node->parent = A.entity;
-        A.node->child_1 = F.entity;
-
-
-      }
-      if(BF_swap_score > BG_swap_score) {
-        G.node->parent = A.entity;
-        A.node->child_1 = G.entity;
-      }
-    }
-
-    if(swap_C) {
-      B.node->child_1 = C.entity;
-      C.node->parent = B.entity;
-      if(CD_swap_score < CE_swap_score) {
-        D.node->parent = A.entity;
-        A.node->child_1 = D.entity;
-      }
-      if(CD_swap_score > CE_swap_score) {
-        E.node->parent = A.entity;
-        A.node->child_1 = E.entity;
-      }
-    }
-    
-    parent.node->aabb = alias_AABB2D_union(B.node->aabb, C.node->aabb);
-
-    struct extracted_node prev_A = A;
-    A = _extract(instance, component, parent.node->parent);
-    if(A.node->child_1 == prev_A.entity) {
-      B = A;
-      C = _extract(instance, component, parent.node->child_2);
-    } else {
-      child1 = _extract(instance, component, parent.node->child_1);
-      child2 = prev_parent;
-    }
-  }
-}
 #endif
 
-#endif // include/alias/data_structure/bvh_h_INCLUDED
-
+#endif
