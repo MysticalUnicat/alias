d19ffacbc0e0076eb1e2d6f9cab416a7801a1047
Sarah Burns <mystical.unicat@gmail.com>
2021-06-16 17:02:26 -0700

    auto Wed Jun 16 05:02:26 PM PDT 2021

---
+++ include/alias/data_structure/vector.h
@@ -0,0 +1,10 @@
+#ifndef _ALIAS_DATA_STRUCTURE_VECTOR_H_
+#define _ALIAS_DATA_STRUCTURE_VECTOR_H_
+
+#include <stdint.h>
+
+#define alias_Vector(T) struct { uint32_t capacity; uint32_t length; T * data; }
+
+
+
+#endif
